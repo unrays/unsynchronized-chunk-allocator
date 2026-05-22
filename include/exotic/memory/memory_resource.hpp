5a293867bcf898a07bc17917d38c98885d4dec42
Félix-Olivier Dumas <felixolivierdumas@hotmail.com>
2026-05-22 15:26:12 -0400

    Refactor memory_resource class to exotic::memory namespace

--- include/exotic/memory/memory_resource.hpp
+++ include/exotic/memory/memory_resource.hpp
@@ -1,33 +1,26 @@
-//===-- memory_ressource.hpp ------------------------------------*- C++ -*-===//
-//
-// Part of the Prysma Project, under the GNU GPL v3.0 or later.
-// See LICENSE at the project root for license information.
-// SPDX-License-Identifier: GPL-3.0-or-later WITH Prysma-exception-1.0
-//
-//===----------------------------------------------------------------------===//
+// Copyright (c) May 2026 Félix-Olivier Dumas. All rights reserved.
+// Licensed under the terms described in the LICENSE file
 
 #pragma once
 
 #include <cstddef>
 #include <stdexcept>
-#include "compiler/macros/prysma_nodiscard.h"
-#include "compiler/macros/prysma_unlikely.h"
 
-namespace prysma {
+namespace exotic::memory {
 
 class memory_resource {
 public:
     virtual ~memory_resource() = default;
 
 public:
-    PRYSMA_NODISCARD void* allocate(std::size_t bytes, std::size_t alignment) noexcept {
-        if (alignment == 0 || (alignment & (alignment - 1)) != 0) PRYSMA_UNLIKELY_BRANCH
+    [[nodiscard]] void* allocate(std::size_t bytes, std::size_t alignment) noexcept {
+        if (alignment == 0 || (alignment & (alignment - 1)) != 0) [[unlikely]]
             throw std::invalid_argument("alignment must be a power of two");
         return do_allocate(bytes, alignment);
     }
 
     void deallocate(void* p, std::size_t bytes, std::size_t alignment) noexcept {
-        if (alignment == 0 || (alignment & (alignment - 1)) != 0) PRYSMA_UNLIKELY_BRANCH
+        if (alignment == 0 || (alignment & (alignment - 1)) != 0) [[unlikely]]
             throw std::invalid_argument("alignment must be a power of two");
         do_deallocate(p, bytes, alignment);
     }
